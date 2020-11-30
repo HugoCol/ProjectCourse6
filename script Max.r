890c6a17e7643d3ab226a623924121463ee7ffd4
[Hugo Colenbrander] <hugo.colenbrander@gmail.com>
2020-11-30 17:34:46 +0100

    "Test2?"

--- script Max.r
+++ script Max.r
@@ -7,12 +7,14 @@ require(edgeR)
 # Bestand RNA-seq data inlezen.
 inhoud = read.table("./RNAseq-bestanden/RNA-Seq-counts.txt", header=TRUE)
 row.names(inhoud) = inhoud[,"ID"]
+
 # Annotatiedata toevoegen.
 annotatiebestand = "./RNAseq-bestanden/WCFS1_anno.txt"
 annotatie = read.table(annotatiebestand, header=TRUE, sep="\t"
                   , quote="")
 names(annotatie)[names(annotatie) == "name"] = "ID"
 data = merge(inhoud, annotatie, by="ID")
+
 
 # Data omzetten naar DGEList.
 groep = factor(c("WCFS1.glc.1",	"WCFS1.glc.2",	"WCFS1.rib.1"
@@ -21,12 +23,15 @@ groep = factor(c("WCFS1.glc.1",	"WCFS1.glc.2",	"WCFS1.rib.1"
 specialeLijst = DGEList(counts=data[,2:9], group=groep)
 # Met edgeR de data normaliseren (edgeRUsersGuide()
 # voor gebruiksaanwijzing).
+
+
 x = calcNormFactors(specialeLijst, method="TMM")
 keep = filterByExpr(specialeLijst)
 x = specialeLijst[keep, , keep.lib.sizes=FALSE]
 x$samples$lib.size = colSums(x$counts)
 
 # Dispersie berekenen
+
 #matrix = model.matrix(~0+group, data=x$samples)
 #colnames(matrix) = levels(x$samples$group)
 #dispersie = estimateDisp(x, matrix)
